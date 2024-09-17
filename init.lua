8e4b82514689811b854f5c7199fb137ce1d99576
Demiurg39 <chiryagov2014@gmail.com>
2024-09-17 12:37:41 +0300

    started colemak support

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+-- vim.loader.enable()
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -32,6 +33,7 @@ dofile(vim.g.base46_cache .. "statusline")
 require "options"
 require "nvchad.autocmds"
 require "autocmd"
+require("utils.colemak").setup()
 
 vim.schedule(function()
     require "mappings"
