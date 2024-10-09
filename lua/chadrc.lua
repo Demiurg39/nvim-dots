27b80f6fc5f46e882a34a167b8f11c32a7b104ae
Demiurg39 <chiryagov2014@gmail.com>
2024-10-09 12:45:12 +0300

    nvchad ui plugin setup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,54 +1,86 @@
 -- This file needs to have same structure as nvconfig.lua
--- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
 
----@diagnostic disable-next-line: undefined-doc-name
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-    ------------------------------- base46 -------------------------------------
-    theme = "onedark",
+M.base46 = {
+    theme = "catppuccin",
+    transparency = true,
+
     hl_override = {
         Comment = { italic = true },
         ["@comment"] = { italic = true },
     },
+
     integrations = {
         "blankline",
         "cmp",
         "git",
     },
-    changed_themes = {},
-    transparency = true,
+}
 
+M.ui = {
     cmp = {
-        icons = true,
+        icons_left = true, -- only for non-atom styles!
         lspkind_text = true,
-        style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+        style = "flat_dark", -- default/flat_light/flat_dark/atom/atom_colored
+        format_colors = {
+            tailwind = true, -- will work for css lsp too
+            icon = "󱓻",
+        },
     },
 
-    telescope = { style = "borderless" }, -- borderless / bordered
-
-    ------------------------------- nvchad_ui modules -----------------------------
     statusline = {
-        theme = "default", -- default/vscode/vscode_colored/minimal
+        enabled = true,
+        theme = "minimal", -- default/vscode/vscode_colored/minimal
         -- default/round/block/arrow separators work only for default statusline theme
         -- round and block will work for minimal theme only
-        separator_style = "default",
+        separator_style = "round",
         order = nil,
         modules = nil,
     },
 
-    -- lazyload it when there are 1+ buffers
     tabufline = {
         enabled = true,
         lazyload = true,
         order = { "treeOffset", "buffers", "tabs", "btns" },
         modules = nil,
     },
+}
 
-    cheatsheet = { theme = "grid" }, -- simple/grid
+M.nvdash = {
+    load_on_startup = true,
+    header = require("utils.dashboards").ayanami_rei,
+    buttons = {
+        { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+        { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
+        { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
+        { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
 
-    lsp = { signature = true },
+        { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashLazy",
+            no_gap = true,
+        },
+
+        { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+    },
+}
+
+M.colorify = {
+    enabled = true,
+    mode = "virtual", -- fg, bg, virtual
+    virt_text = "󱓻 ",
+    highlight = { hex = true, lspvars = true },
 }
 
 return M
