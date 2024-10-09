1334cdb8d911e0d99fca9a2e853fd0afbe8af86e
Demiurg39 <chiryagov2014@gmail.com>
2024-10-09 12:47:01 +0300

    code refactored

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,43 +1,31 @@
-require("neoconf").setup()
+-- require("neoconf").setup()
+
+-- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-local on_attach = require("utils.lsp").on_attach -- For custom mappings
-
-lspconfig.servers = { "lua_ls", "clangd", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(lspconfig.servers) do
-    lspconfig[lsp].setup {
-        on_attach = on_attach,
-        on_init = nvlsp.on_init,
-        capabilities = nvlsp.capabilities,
-    }
+local servers = {
+  html = {},
+  cssls = {},
+  clangd = {},
+  lua_ls = {},
+}
+
+for name, opts in pairs(servers) do
+  opts.on_init = nvlsp.on_init
+  opts.on_attach = nvlsp.on_attach
+  opts.capabilities = nvlsp.capabilities
+
+  lspconfig[name].setup(opts)
 end
 
--- configuring single servers
 lspconfig.clangd.setup {
     on_attach = function(client, buffnr)
         client.server_capabilities.documentFormattingProvider = false
         client.server_capabilities.documentRangeFormattingProvider = false
 
-        on_attach(client, buffnr)
-    end,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-}
-
-lspconfig.pyright.setup {
-    on_attach = on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-
-    settings = {
-        python = {
-            analysis = {
-                typeCheckingMode = "off", -- Disable type checking diagnostics
-            },
-        },
-    },
+        nvlsp.on_attach(client, buffnr)
+    end
 }
